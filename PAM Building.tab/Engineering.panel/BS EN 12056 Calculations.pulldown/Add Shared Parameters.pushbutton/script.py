6746c7f9b012089bde67dcd3484241cc8de18c26
innovazzieng <bogdan.pbc@gmail.com>
2024-10-21 20:08:28 +0300

    Updated url

--- PAM Building.tab/Engineering.panel/BS EN 12056 Calculations.pulldown/Add Shared Parameters.pushbutton/script.py
+++ PAM Building.tab/Engineering.panel/BS EN 12056 Calculations.pulldown/Add Shared Parameters.pushbutton/script.py
@@ -79,7 +79,7 @@ def show_window():
     
     def on_image_click(sender, event_args):
         """Handle the image click event to open a URL."""
-        url = "www.innovazzi.com"
+        url = "https://www.pambuilding.co.uk"
         webbrowser.open(url)
 
     def header_drag(sender, event_args):
